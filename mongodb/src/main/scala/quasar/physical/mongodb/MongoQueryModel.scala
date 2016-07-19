15cc512e215e7b8abd753458ba4e7ff5cbdbfa27
Moss Prescott <moss@theprescotts.com>
2016-07-19 16:42:44 -0400

    Semigroup[MongoQueryModel]

--- mongodb/src/main/scala/quasar/physical/mongodb/MongoQueryModel.scala
+++ mongodb/src/main/scala/quasar/physical/mongodb/MongoQueryModel.scala
@@ -14,16 +14,31 @@
  * limitations under the License.
  */
 
+package quasar.physical.mongodb
+
+import scalaz.Semigroup
+
 sealed trait MongoQueryModel
 
 object MongoQueryModel {
   /** The oldest supported version. */
-  final case object `2.6`
+  case object `2.6` extends MongoQueryModel
 
   /** Adds a few operators. */
-  final case object `3.0`
+  case object `3.0` extends MongoQueryModel
 
   /** Adds `$lookup` and `$distinct`, several new operators, and makes
     * accumulation operators available in `$project`. */
-  final case object `3.2`
+  case object `3.2` extends MongoQueryModel
+
+  implicit val semigroup: Semigroup[MongoQueryModel] =
+    new Semigroup[MongoQueryModel] {
+      def append(f1: MongoQueryModel, f2: => MongoQueryModel) = (f1, f2) match {
+        case (`3.2`, _) => `3.2`
+        case (_, `3.2`) => `3.2`
+        case (`3.0`, _) => `3.0`
+        case (_, `3.0`) => `3.0`
+        case (`2.6`, _) => `2.6`
+      }
+    }
 }
